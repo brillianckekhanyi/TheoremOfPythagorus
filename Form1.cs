3c0e10de9e793432afdcf18d5c514949468e08ac
Dell <Dell@DESKTOP-HKF2PQO>
2024-01-24 17:53:14 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TheoremOfPythagorus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a, b;
+            a = Convert.ToInt32(textBox1.Text);
+            b = Convert.ToInt32(textBox2.Text);
+           double Hypotenuse = Math.Sqrt((Math.Pow(a,2)+Math.Pow(b,2)));
+
+            textBox3.Text = Hypotenuse.ToString("n");
+        }
+    }
+}
